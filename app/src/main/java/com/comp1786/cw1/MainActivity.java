44b9fd6a680975e0b9c9aeed042027b2d641fdbb
Alanle AXO <lephuc@axo.business>
2023-10-14 15:25:08 +0700

    test git

--- app/src/main/java/com/comp1786/cw1/MainActivity.java
+++ app/src/main/java/com/comp1786/cw1/MainActivity.java
@@ -16,8 +16,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //Dm mày phúc ơi
     }
 
     public void handleButtonClick(View v) {
