3775e9d677809739c291e70d8308a48aa40cb957
peguin818 <pcc1327@gmail.com>
2023-10-15 13:10:39 +0700

    Final Layout, Unfinished database

--- app/src/main/java/com/comp1786/cw1/MainActivity.java
+++ app/src/main/java/com/comp1786/cw1/MainActivity.java
@@ -2,43 +2,86 @@ package com.comp1786.cw1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+    final Calendar myCalendar= Calendar.getInstance();
+    EditText editHikeName;
+    EditText editLocation;
+    EditText editDate;
+    EditText editLength;
+    EditText editEContact;
+    EditText editDescription;
+
+    RadioGroup groupPark;
+    RadioGroup groupDifficulty;
+
+    Button btnTest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Dm mày phúc ơi
+        editDate = (EditText) findViewById(R.id.editDate);
+        DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, month);
+                myCalendar.set(Calendar.DAY_OF_MONTH, day);
+                updateDateLabel();
+            }
+        };
+        editDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(MainActivity.this, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+//
+//        btnTest.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                verifyIfEditTextIsFilled(editHikeName, editLocation, editDate, editLength, editDescription);
+//                verifyIfRadioGroupIsChecked(groupPark, groupDifficulty, groupVehicle);
+//            }
+//        });
     }
 
     public void handleButtonClick(View v) {
-        EditText editHikeName = (EditText) findViewById(R.id.editHikeName);
-        EditText editLocation = (EditText) findViewById(R.id.editLocation);
-        EditText editDate = (EditText) findViewById(R.id.editDate);
-        EditText editLength = (EditText) findViewById(R.id.editLength);
-        EditText editDescription = (EditText) findViewById(R.id.editDescription);
+        editHikeName = findViewById(R.id.editHikeName);
+        editLocation = findViewById(R.id.editLocation);
+        editDate = findViewById(R.id.editDate);
+        editLength = findViewById(R.id.editLength);
+        editEContact = findViewById(R.id.editEContact);
+        editDescription = findViewById(R.id.editDescription);
 
-        RadioGroup groupPark = (RadioGroup) findViewById(R.id.radioParking);
-        RadioGroup groupDifficulty = (RadioGroup) findViewById(R.id.radioDifficulty);
-        RadioGroup groupVehicle = (RadioGroup) findViewById(R.id.radioGroupVehicle);
+        groupPark = findViewById(R.id.radioParking);
+        groupDifficulty = findViewById(R.id.radioDifficulty);
 
-        String strHikeName = editHikeName.getText().toString();
-        String strLocation = editLocation.getText().toString();
-        String strDate = editDate.getText().toString();
-        String strLength = editLength.getText().toString();
-        String strDescription = editDescription.getText().toString();
+        verifyIfEditTextIsFilled(editHikeName, editLocation, editDate, editLength, editEContact, editDescription);
+        verifyIfRadioGroupIsChecked(groupPark, groupDifficulty);
+    }
 
-        verifyIfEditTextIsFilled(editHikeName, editLocation, editDate, editLength, editDescription);
-        verifyIfRadioGroupIsChecked(groupPark, groupDifficulty, groupVehicle);
+    private void updateDateLabel(){
+        String myFormat="dd/MM/yyyy";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.ROOT);
+        editDate.setText(dateFormat.format(myCalendar.getTime()).toString());
     }
 
     private boolean verifyIfEditTextIsFilled(EditText... editText) {
---
+++ app/src/main/java/com/comp1786/cw1/dbHelper/HikeDbHelper.java
@@ -0,0 +1,7 @@
+package com.comp1786.cw1.dbHelper;
+
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class HikeDbHelper extends SQLiteOpenHelper {
+    
+}
