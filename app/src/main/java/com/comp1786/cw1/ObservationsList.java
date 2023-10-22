bb3c571a6c643dde78a5c7811b1e14dde76a45cb
MarkTaylor1412 <minhtan14122013@gmail.com>
2023-10-22 20:37:47 +0700

    MHIKE-21: add input form obs-list.

---
+++ app/src/main/java/com/comp1786/cw1/ObservationsList.java
@@ -0,0 +1,16 @@
+package com.comp1786.cw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.comp1786.cw1.R;
+
+public class ObservationsList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_observations_list);
+    }
+}
