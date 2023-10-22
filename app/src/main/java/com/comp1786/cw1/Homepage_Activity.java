2c97b91eac42fa1999b6a657aaf9f11568b09c2b
M1dnight00 <newjourney20102003@gmail.com>
2023-10-22 16:18:02 +0700

    add homepage

---
+++ app/src/main/java/com/comp1786/cw1/Homepage_Activity.java
@@ -0,0 +1,21 @@
+package com.comp1786.cw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Homepage_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homepage);
+    }
+    public void handleButtonClick (View view)
+    {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+}
