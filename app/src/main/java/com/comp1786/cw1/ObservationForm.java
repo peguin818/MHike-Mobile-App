4cc018cc012b392c81cd6cc587d7af7decd1b21c
MarkTaylor1412 <minhtan14122013@gmail.com>
2023-11-04 17:09:58 +0700

    MHIKE-21: 17:09.

---
+++ app/src/main/java/com/comp1786/cw1/ObservationForm.java
@@ -0,0 +1,14 @@
+package com.comp1786.cw1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ObservationForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_observation_form);
+    }
+}
